5c34de8b16b0c48a76bfcb8ccf48af2426ddb4e9
Karl Sandwich <karl.sandwich@gmail.com>
2015-05-25 14:28:05 -0700

    Add a test default bootstrap button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log('foo start');
 
+
 var fooSquares = require('./fooSquares');
 var barBear = require('./bar');
 var baz = require('./baz');
@@ -10,3 +11,6 @@ console.log('input: ', input);
 console.log('output: ', fooSquares([6]) );
 console.log('output: ', barBear('foo') );
 console.log('output: ', baz() );
+
+// load all of bootstrap's jquery plugins onto the jquery object.
+require('bootstrap');
